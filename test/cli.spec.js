9355ae14c574d5021e8b985b31e1596b094eafc5
Michel Kraemer <michel@undercouch.de>
2014-11-16 11:21:23 +0100

    More tests for the CLI and exit code

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -1,22 +1,32 @@
+var constants = require("../lib/constants");
 var mocks = require("mocks");
 
 describe("frictionless cli", function() {
     var frictionless;
     var cli;
+    var log;
 
     beforeEach(function() {
+        log = jasmine.createSpy("log");
+
         // mock 'frictionless' module required by 'cli' module
         frictionless = jasmine.createSpy("frictionless").and.returnValue([]);
         cli = mocks.loadFile(__dirname + "/../lib/cli", {"./index": frictionless}).module.exports;
     });
 
     it("should call frictionless with current directory", function() {
-        cli({_: []});
+        expect(cli({_: []}, log)).toBe(0);
         expect(frictionless).toHaveBeenCalledWith(["."]);
     });
 
     it("should call frictionless with destination directory", function() {
-        cli({_: ['test']});
+        expect(cli({_: ['test']}, log)).toBe(0);
         expect(frictionless).toHaveBeenCalledWith(["test"]);
+    });
+
+    it("should display version and exit", function() {
+        expect(cli({version: true}, log)).toBe(0);
+        expect(log).toHaveBeenCalledWith(constants.NAME + " " + constants.VERSION);
+        expect(frictionless).not.toHaveBeenCalled();
     });
 });
